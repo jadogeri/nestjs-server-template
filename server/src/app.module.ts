fd4e599471af17425fb72c8dd1c05b23e76805f4
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-02-14 15:22:55 -0600

    Added auth feature

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
 import { SessionModule } from './modules/session/session.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [UserModule, SessionModule],
+  imports: [UserModule, SessionModule, AuthModule],
   controllers: [],
   providers: [],
 })
