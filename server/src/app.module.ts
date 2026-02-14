d6aec1276b4bc1870b9254d0644e92457e33e184
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-02-14 15:23:46 -0600

    Added profile feature

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
 import { SessionModule } from './modules/session/session.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { ProfileModule } from './modules/profile/profile.module';
 
 @Module({
-  imports: [UserModule, SessionModule, AuthModule],
+  imports: [UserModule, SessionModule, AuthModule, ProfileModule],
   controllers: [],
   providers: [],
 })
