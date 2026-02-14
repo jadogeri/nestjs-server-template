f9282a052dbba166936d16591ef98fd3003576db
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-02-14 15:22:20 -0600

    Added session feature

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
+import { SessionModule } from './modules/session/session.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, SessionModule],
   controllers: [],
   providers: [],
 })
