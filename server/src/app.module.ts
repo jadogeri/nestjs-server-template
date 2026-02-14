bf80f82190b598c8f52b712062e56194d24f1fec
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-02-14 15:26:33 -0600

    Added role feature

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { UserModule } from './modules/user/user.module';
 import { SessionModule } from './modules/session/session.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ProfileModule } from './modules/profile/profile.module';
+import { RoleModule } from './modules/role/role.module';
 
 @Module({
-  imports: [UserModule, SessionModule, AuthModule, ProfileModule],
+  imports: [UserModule, SessionModule, AuthModule, ProfileModule, RoleModule],
   controllers: [],
   providers: [],
 })
