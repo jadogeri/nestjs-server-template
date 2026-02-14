93601398370a2395fa4559963f3a9e43f1e589d5
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-02-14 15:30:34 -0600

    Added all features

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './modules/auth/auth.module';
 import { ProfileModule } from './modules/profile/profile.module';
 import { RoleModule } from './modules/role/role.module';
 import { ContactModule } from './modules/contact/contact.module';
+import { PermissionModule } from './modules/permission/permission.module';
 
 @Module({
-  imports: [UserModule, SessionModule, AuthModule, ProfileModule, RoleModule, ContactModule],
+  imports: [UserModule, SessionModule, AuthModule, ProfileModule, RoleModule, ContactModule, PermissionModule],
   controllers: [],
   providers: [],
 })
