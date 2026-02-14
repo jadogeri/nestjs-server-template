441cc8025105d2688e868c7196d1cc9560886428
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-02-14 16:53:32 -0600

    modified auth.module

--- server/src/modules/auth/auth.module.ts
+++ server/src/modules/auth/auth.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { AuthRepository } from './auth.repository';
+//modules
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+import { Auth } from './entities/auth.entity';
+
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Auth])],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AuthRepository],
+  exports: [AuthService, AuthRepository],
 })
 export class AuthModule {}
+
+  
