ef2c2c111d7e54e2d7796ef8a64d6a07cdd42474
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-02-14 16:16:02 -0600

    Added typeorm config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,8 +7,21 @@ import { RoleModule } from './modules/role/role.module';
 import { ContactModule } from './modules/contact/contact.module';
 import { PermissionModule } from './modules/permission/permission.module';
 
+import dataSourceOptions from './configs/type-orm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-  imports: [UserModule, SessionModule, AuthModule, ProfileModule, RoleModule, ContactModule, PermissionModule],
+  imports: [
+    UserModule, 
+    SessionModule, 
+    AuthModule, 
+    ProfileModule, 
+    RoleModule, 
+    ContactModule, 
+    PermissionModule,
+    TypeOrmModule.forRoot({ ...dataSourceOptions, autoLoadEntities: true }),
+
+  ],
   controllers: [],
   providers: [],
 })
