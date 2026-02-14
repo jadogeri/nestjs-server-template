a570f9219d171d13f8fb6242d75d631811a58551
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-02-14 17:03:14 -0600

    modified profile module

--- server/src/modules/profile/profile.module.ts
+++ server/src/modules/profile/profile.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { ProfileController } from './profile.controller';
+import { ProfileRepository } from './profile.repository';
+import { Profile } from './entities/profile.entity';
 
+// Other modules
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+
+
+  
 @Module({
+  imports: [TypeOrmModule.forFeature([Profile])],
   controllers: [ProfileController],
-  providers: [ProfileService],
+  providers: [ProfileService, ProfileRepository],
+  exports: [ProfileService, ProfileRepository],
 })
 export class ProfileModule {}
+
+  
