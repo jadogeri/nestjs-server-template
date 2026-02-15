e50eb95eb39c2af2a489c9e6bc057075a13be227
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-02-15 05:44:05 -0600

    Updated Profile entity

--- server/src/modules/profile/entities/profile.entity.ts
+++ server/src/modules/profile/entities/profile.entity.ts
@@ -1,7 +1,40 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn, UpdateDateColumn } from "typeorm";
+import { User } from "../../user/entities/user.entity";
+import { Location } from "../../../common/entities/location.entity";
 
-@Entity()
-export class Profile {
-    @PrimaryGeneratedColumn()   
+@Entity("profiles")
+export class Profile extends BaseEntity{
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column({ nullable: true })
+    bio: string;
+
+    @Column({ nullable: true })
+    avatarUrl: string;
+
+    @Column(() => Location) 
+    location?: Location;
+
+    @Column({ nullable: true })
+    website: string;
+
+    @Column({ nullable: true })
+    socialMedia: string;
+
+    @Column({ nullable: true })
+    gender: string;
+
+    // Use "simple-json" to store preferences as an object (e.g., { theme: 'dark' })
+    @Column("simple-json", { nullable: true })
+    preferences: { [key: string]: any };
+
+    @UpdateDateColumn()
+    updatedAt: Date; // Automatically updated on each save()
+
+    // Foreign Key setup
+    @OneToOne(() => User, (user) => user.profile)
+    @JoinColumn({ name: "userId" })
+    user: User;
+
 }
