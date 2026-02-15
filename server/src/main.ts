e0c00c206953aa6d09440b00d25e05c05dcb5f63
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-02-15 04:01:12 -0600

    updated main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,16 +3,16 @@ import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { swaggerConfig } from './configs/swagger.config';
 
+class Server {
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  static async bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
 
-  SwaggerModule.setup('docs', app, document ); // The documentation will be available at http://localhost:3000/docs
+    SwaggerModule.setup('docs', app, document ); // The documentation will be available at http://localhost:3000/docs
 
-
-  await app.listen(process.env.PORT ?? 3000);
+    await app.listen(process.env.PORT ?? 3000);
+  }
 }
-bootstrap();
 
-
+Server.bootstrap();
