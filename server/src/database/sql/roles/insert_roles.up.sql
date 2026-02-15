6841596ad1bbfc54c61333c566df79585a6c750f
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-02-15 04:05:04 -0600

    Added sql scripts

---
+++ server/src/database/sql/roles/insert_roles.up.sql
@@ -0,0 +1,29 @@
+-- ADMIN
+INSERT INTO "roles" ("name", "description")
+SELECT 'admin', 'Administrator with full system access'
+WHERE NOT EXISTS (SELECT 1 FROM "roles" WHERE "name" = 'admin');
+
+-- USER
+INSERT INTO "roles" ("name", "description")
+SELECT 'user', 'Standard registered user'
+WHERE NOT EXISTS (SELECT 1 FROM "roles" WHERE "name" = 'user');
+
+-- GUEST
+INSERT INTO "roles" ("name", "description")
+SELECT 'guest', 'Unregistered user with limited access'
+WHERE NOT EXISTS (SELECT 1 FROM "roles" WHERE "name" = 'guest');
+
+-- EDITOR
+INSERT INTO "roles" ("name", "description")
+SELECT 'editor', 'Can create and edit content but cannot manage users'
+WHERE NOT EXISTS (SELECT 1 FROM "roles" WHERE "name" = 'editor');
+
+-- VIEWER
+INSERT INTO "roles" ("name", "description")
+SELECT 'viewer', 'Read-only access to the system'
+WHERE NOT EXISTS (SELECT 1 FROM "roles" WHERE "name" = 'viewer');
+
+-- SUPER USER
+INSERT INTO "roles" ("name", "description")
+SELECT 'super user', 'Higher level user with elevated privileges'
+WHERE NOT EXISTS (SELECT 1 FROM "roles" WHERE "name" = 'super user');
