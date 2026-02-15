2c239a356678c17d738818fdce0ac092bd8b6a5f
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-02-15 03:18:50 -0600

    Updated ProfileController class

--- server/src/modules/profile/profile.controller.ts
+++ server/src/modules/profile/profile.controller.ts
@@ -5,6 +5,7 @@ import { UpdateProfileDto } from './dto/update-profile.dto';
 
 @Controller('profile')
 export class ProfileController {
+
   constructor(private readonly profileService: ProfileService) {}
 
   @Post()
