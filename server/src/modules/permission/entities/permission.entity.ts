82ecfa6bde5c965b6d94ffcb5248c4470f4de853
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-02-15 05:42:58 -0600

    Updated Permission entity

--- server/src/modules/permission/entities/permission.entity.ts
+++ server/src/modules/permission/entities/permission.entity.ts
@@ -1,7 +1,31 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique, ManyToMany } from 'typeorm';
+import { Resource } from '../../../common/enums/resource.enum';
+import { Action } from '../../../common/enums/action.enum';
+import { Role } from '../../../modules/role/entities/role.entity';
 
-@Entity()
-export class Permission {
-    @PrimaryGeneratedColumn()   
-    id: number;
+@Entity('permissions')
+@Unique(['resource', 'action']) // Composite unique constraint
+export class Permission extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'simple-enum',
+    enum: Resource,
+    default: Resource.AUTH,
+  })
+  resource: Resource;
+
+  @Column({
+    type: 'simple-enum',
+    enum: Action,
+    default: Action.READ,
+  })
+  action: Action;
+
+  @Column({ type: 'varchar', nullable: true })
+  description: string;
+
+  @ManyToMany(() => Role, (role: Role) => role.permissions)
+  roles: Role[];    
 }
