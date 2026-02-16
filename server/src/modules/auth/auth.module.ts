f79f97d35fa6bb21721f19c48246e1509ddee3bf
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-02-15 20:52:28 -0600

    Added LocalStrategy

--- server/src/modules/auth/auth.module.ts
+++ server/src/modules/auth/auth.module.ts
@@ -1,21 +1,31 @@
+// 1. NestJS & Framework Imports
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+
+// 2. Services (Logic Layer)
 import { AuthService } from './auth.service';
+import { PayloadMapperService } from './payload-mapper.service';
+
+// 3. Controllers (Presentation Layer)
 import { AuthController } from './auth.controller';
+
+// Repositories & Entities
 import { AuthRepository } from './auth.repository';
 import { Auth } from './entities/auth.entity';
 
-// Other modules
-import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+// modules
 import { SessionModule } from '../session/session.module';
 import { RoleModule } from '../role/role.module';
 import { UserModule } from '../user/user.module';
-import { PayloadMapperService } from './payload-mapper.service';
+
+// Strategies
+import { LocalStrategy } from './strategies/local.strategy';
 
 
 @Module({
   imports: [ UserModule, RoleModule, SessionModule, TypeOrmModule.forFeature([Auth])],
   controllers: [AuthController],
-  providers: [AuthService, AuthRepository, PayloadMapperService],
+  providers: [AuthService, AuthRepository, PayloadMapperService, LocalStrategy],
   exports: [AuthService, AuthRepository, PayloadMapperService],
 })
 export class AuthModule {}
