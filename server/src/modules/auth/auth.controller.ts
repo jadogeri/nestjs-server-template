6f67f60c6323b2b5243d947c699f461a0a2b00de
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-02-17 00:52:37 -0600

    Updated refreshToken function

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -32,6 +32,8 @@ import type { AccessTokenPayload } from '../..//common/types/access-token-payloa
 import { AccessAuthGuard } from '../..//core/security/guards/access-auth.guard';
 import { AccessToken } from '../../common/decorators/access-token.decorator';
 import { ApiRefresh } from './decorators/api-refresh-token.decorator';
+import type { RefreshTokenPayload } from 'src/common/types/refresh-token-payload.type';
+import { RefreshToken } from 'src/common/decorators/refresh-token.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -87,8 +89,9 @@ export class AuthController {
   }
 
   @Post('/refresh')
+
   @ApiRefresh()
-  async refreshToken(@Req() req: Request,@Res({ passthrough: true }) res: Response): Promise<any> {
+  async refreshToken(@RefreshToken() refreshToken: RefreshTokenPayload, @Req() req: Request,@Res({ passthrough: true }) res: Response): Promise<any> {
 
   //@UseGuards(RefreshAuthGuard) // Use the custom refresh token guard
   //async refreshToken(@Req() req: Request,@Res({ passthrough: true }) res: Response): Promise<any> {
@@ -97,7 +100,6 @@ export class AuthController {
     //console.log("jwt payload: " + JSON.stringify(jwtPayload));
     //return this.authService.refreshTokens(req, res, jwtPayload);
   // Use the cookie name you set, e.g., 'refreshToken'
-  const refreshToken = req.cookies['refreshToken']; 
   console.log("Cookie received:", refreshToken);
   
   return { message: "Refresh token retrieved from cookie" };  }
