66d4e7f118ab178cfd522ee936242ef74e22c08e
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-02-20 20:38:56 -0600

    Updated CreateContactDto

--- server/src/modules/contact/dto/create-contact.dto.ts
+++ server/src/modules/contact/dto/create-contact.dto.ts
@@ -1,10 +1,15 @@
-import { IsString, IsEmail, IsOptional, IsPhoneNumber, IsNotEmpty } from 'class-validator';
+import { IsString, IsOptional, IsPhoneNumber, IsNotEmpty, Matches } from 'class-validator';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsFaxNumber } from '../../../common/decorators/validators/is-fax-number.decorator';
+import { IsUserEmail } from 'src/common/decorators/validators/is-email.decorator';
 
 export class CreateContactDto {
   @ApiProperty({ example: 'Jane Smith' })
   @IsString()
   @IsNotEmpty()
+ @Matches(/^[a-zA-Z ]+$/, {
+    message: 'fullName must only contain letters (a-z, A-Z) and spaces',
+  })
   fullName: string;
 
   @ApiPropertyOptional({ example: '+15559876543' })
@@ -14,11 +19,22 @@ export class CreateContactDto {
 
   @ApiPropertyOptional({ example: 'jane@company.com' })
   @IsOptional()
-  @IsEmail()
+  @IsUserEmail()
   email?: string | null;
 
-  @ApiPropertyOptional({ example: '+15551112222' })
+  @ApiPropertyOptional({ example: ['15551112222', '+1-555-333-4444'] })
   @IsOptional()
-  @IsPhoneNumber()
+  @IsFaxNumber()
   fax?: string | null;
+
+  
+  @ApiPropertyOptional({ example: {
+    address    : '456 Elm St',
+    city        : 'Othertown',
+    state       : 'NY',
+    postalCode  : '67890',
+    country     : 'US'
+  }})
+  @IsOptional()
+  location: Location | null;
 }
