c378f26dd8fcb806d478cf0833f7d45223948067
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-02-21 22:22:08 -0600

    Added RolesGuard and PermissionsGuard

--- server/src/core/security/guards/permissions.guard.ts
+++ server/src/core/security/guards/permissions.guard.ts
@@ -1,6 +1,27 @@
-// permissions.decorator.ts
-import { SetMetadata } from '@nestjs/common';
-import { PermissionString } from '../../../common/types/permission-string.type';
+import { CanActivate, ExecutionContext } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { Service } from "../../../common/decorators/service.decorator";
+import { PermissionString } from "../../../common/types/permission-string.type";
+import { PERMISSIONS_KEY } from "../../../common/decorators/permissions.decorator";
 
-export const PERMISSIONS_KEY = 'permissions';
-export const Permissions = (...permissions: PermissionString[]) => SetMetadata(PERMISSIONS_KEY, permissions);
+@Service()
+export class PermissionsGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredPermissions = this.reflector.getAllAndOverride<PermissionString[]>(PERMISSIONS_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (!requiredPermissions) return true;
+
+    const { user } = context.switchToHttp().getRequest();
+
+    console.log("user ==> ", user);
+    console.log("requiredPermissions ==> ", requiredPermissions);
+    
+    return requiredPermissions.every((permission) => 
+      user.permissions?.includes(permission) || user.permissions?.includes('*:*')
+    );
+  }
+}
--- server/src/core/security/guards/roles.guard.ts
+++ server/src/core/security/guards/roles.guard.ts
@@ -1,6 +1,24 @@
-// roles.decorator.ts
-import { SetMetadata } from '@nestjs/common';
-import { UserRole } from '../../../common/enums/user-role.enum';
+import { CanActivate, ExecutionContext } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { ROLES_KEY } from "../../../common/decorators/roles.decorator";
+import { UserRole } from "../../../common/enums/user-role.enum";
+import { Service } from "../../../common/decorators/service.decorator";
 
-export const ROLES_KEY = 'roles';
-export const Roles = (...roles: UserRole[]) => SetMetadata(ROLES_KEY, roles);
+@Service()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+    
+  canActivate(context: ExecutionContext): boolean {
+    // Retrieves the array of roles defined on the handler
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    
+    if (!requiredRoles) return true;
+
+    const { user } = context.switchToHttp().getRequest();
+    // Check if user has ANY of the required roles
+    return requiredRoles.some((role) => user.roles?.includes(role));
+  }
+}
