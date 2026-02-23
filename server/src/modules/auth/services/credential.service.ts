2967d2e3359a9155b0aeba59c53a20c89568d062
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2026-02-23 12:02:45 -0600

    Removed commented code

--- server/src/modules/auth/services/credential.service.ts
+++ server/src/modules/auth/services/credential.service.ts
@@ -74,31 +74,7 @@ export class CredentialService implements CredentialServiceInterface {
             sessionId: sessionId // Return it if needed by the client
         };  
     }
-/**
- export const logoutUser = asyncHandler(async (req: Request, res : Response) => {
-   
-  const {token} :IUserAuthorized = req.body
-  console.log("token==================", token)
-  if(!token){ 
-    errorBroadcaster(res,400,"field token is mandatory");
-  } 
-  const authenticatedUser = await authService.getByToken(token)
-  if(!authenticatedUser){
-    res.status(401).json({ message: "Already logged out" });
-  }
-  //remove auth from Auth Collection
-  
-  let logoutAuth : IAuth = {
-    token : token
-  }
-  await authService.remove(logoutAuth)
 
-
-  
-  res.status(200).json({ message: "logout the user" });
-});
-
- */
   async logout(res: Response<any, Record<string, any>>, refreshTokenPayload: RefreshTokenPayload): Promise<any> {
 
     const { userId, sessionId } = refreshTokenPayload;
