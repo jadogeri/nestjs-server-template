800687db026cd1043faf6563697ac0d50b28c736
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2026-02-26 12:12:37 -0600

    updated app.module.ts

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -13,6 +13,7 @@ import { CoreModule } from './core/core.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'node:path';
 
+
 console.log("__dirname:", __dirname);
 
  @Module({
