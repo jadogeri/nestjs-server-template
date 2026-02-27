8201eedd46dbbcf08234198d71c8b4616b9728aa
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2026-02-27 12:30:25 -0600

    Updated CreatePermissionDto

--- server/src/modules/permission/dto/create-permission.dto.ts
+++ server/src/modules/permission/dto/create-permission.dto.ts
@@ -1,1 +1,34 @@
-export class CreatePermissionDto {}
+import { IsEnum, IsOptional, IsString, IsNotEmpty } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { Resource } from '../../../common/enums/resource.enum';
+import { Action } from '../../../common/enums/action.enum';
+
+export class CreatePermissionDto {
+  @ApiProperty({ 
+    enum: Resource, 
+    example: Resource.USER,
+    description: 'The system resource this permission applies to' 
+  })
+  @IsEnum(Resource)
+  @IsNotEmpty()
+  resource: Resource;
+
+  @ApiProperty({ 
+    enum: Action, 
+    example: Action.CREATE,
+    description: 'The specific operation allowed on the resource' 
+  })
+  @IsEnum(Action)
+  @IsNotEmpty()
+  action: Action;
+
+  @ApiPropertyOptional({ 
+    example: 'Allows the user to create new user accounts.',
+    description: 'A human-readable description of what this permission grants'
+  })
+  @IsOptional()
+  @IsString()
+  @Transform(({ value }) => (typeof value === 'string' ? value.trim() : value))
+  description?: string;
+}
