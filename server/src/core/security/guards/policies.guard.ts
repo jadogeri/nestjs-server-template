99a3051707c3d8612d7c41420faac21bc8cbe4a9
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2026-03-02 09:31:28 -0600

    Added PoliciesGuard

---
+++ server/src/core/security/guards/policies.guard.ts
@@ -0,0 +1,31 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Service } from '../../../common/decorators/service.decorator';
+import { Action } from '../../../common/enums/action.enum';
+import { CaslAbilityFactory } from '../casl/casl-ability.service';
+
+
+interface RequiredPermission {
+  action: Action;
+  resource: string;
+}
+
+@Service()
+export class PoliciesGuard implements CanActivate {
+  constructor(
+    private reflector: Reflector,
+    private caslAbilityFactory: CaslAbilityFactory,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const rules = this.reflector.get<RequiredPermission[]>('permissions', context.getHandler()) || [];
+    
+    const { user } = context.switchToHttp().getRequest();
+    if (!user) return false;
+
+    const ability = this.caslAbilityFactory.createForUser(user);
+    
+    // Check if user has permission for every rule defined on the route
+    return rules.every((rule) => ability.can(rule.action, rule.resource as any));
+  }
+}
