86bd0aac86a3c22336fa3d75c793bcb2b4dabb8f
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-03-02 07:22:14 -0600

    Updated react admin components

--- server/src/modules/stats/stats.controller.ts
+++ server/src/modules/stats/stats.controller.ts
@@ -3,20 +3,20 @@ import { Controller, Get } from '@nestjs/common';
 import { ApiExcludeController } from '@nestjs/swagger';
 import { StatsService } from './stats.service';
 
-//@ApiExcludeController()
+@ApiExcludeController()
 @Controller('stats')
 export class StatsController {
-  constructor(private readonly statsService: StatsService) {}
+    constructor(private readonly statsService: StatsService) {}
 
-  @Get('role-distribution')
-  getRoleDistribution() {
-    return this.statsService.getRoleDistribution();
-  }
+    @Get('role-distribution')
+    getRoleDistribution() {
+        return this.statsService.getRoleDistribution();
+    }
 
-  @Get('summary')
-  getSummary() {
-    return this.statsService.getGeneralStats();
-  }
+    @Get('summary')
+    getSummary() {
+        return this.statsService.getGeneralStats();
+    }
 
     @Get('contacts')
     getContactsDashboard() {
