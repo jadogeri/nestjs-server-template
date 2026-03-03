9fbf3095066c263b4ce5ddcd1586ddd76a1b32ab
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-03-03 00:24:12 -0600

    Added physical_diagram.png

--- server/data-source.ts
+++ server/data-source.ts
@@ -4,6 +4,6 @@ import dataSourceOptions from './src/configs/type-orm.config';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
 // Do NOT export the options here, only the DataSource instance
-const AppDataSource = new DataSource(dataSourceOptions as DataSourceOptions);
+export const AppDataSource = new DataSource(dataSourceOptions as DataSourceOptions);
 
-export default AppDataSource;
+//export default AppDataSource;
