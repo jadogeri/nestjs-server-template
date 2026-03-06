7395dea81262102e48fe116d08c22c2b19b936a5
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-03-06 17:29:42 -0600

    Updated UserController

--- server/src/modules/user/user.controller.ts
+++ server/src/modules/user/user.controller.ts
@@ -1,5 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, UseGuards } from '@nestjs/common';
-import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, UseGuards, UseInterceptors } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -9,42 +8,58 @@ import { ApiGetUsers } from './decorators/api-get-users.decorator';
 import { ApiPatchUser } from './decorators/api-patch-user.decorator';
 import { ApiDeleteUser } from './decorators/api-delete-user.decorator';
 import { AccessAuthGuard } from '../../core/security/guards/access-auth.guard';
+import { CacheMonitorInterceptor } from '../../core/infrastructure/interceptors/cache-monitor.interceptor';
+import type { AccessTokenPayload } from '../../common/types/access-token-payload.type';
+import { AccessToken } from '../../common/decorators/access-token.decorator';
+import { PermissionsGuard } from '../../core/security/guards/permissions.guard';
+import { Roles } from '../../common/decorators/roles.decorator';
+import { Permissions } from '../../common/decorators/permissions.decorator';
+import { RolesGuard } from '../../core/security/guards/roles.guard';
+import { PoliciesGuard } from '../../core/security/guards/policies.guard';
+import { access } from 'fs';
 
-@UseGuards(AccessAuthGuard )
 @Controller('users')
+@UseGuards(AccessAuthGuard, PoliciesGuard ) 
+@UseInterceptors(CacheMonitorInterceptor)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
   @ApiPostUser()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  create(@AccessToken() accessTokenPayload: AccessTokenPayload,@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(accessTokenPayload, createUserDto);
   }
 
   @Get()
+    @UseGuards(PermissionsGuard, RolesGuard) // Apply both guards to this endpoint)
+  @Permissions("all:manage") // Only ADMIN can access this endpoint
+  @Roles('USER', 'SUPER_USER') // Only users with ADMIN role can access this endpoint
   @ApiGetUsers()
-  findAll() {
-    return this.userService.findAll();
+  async findAll(@AccessToken() accessTokenPayload: AccessTokenPayload ) {
+    return await this.userService.findAll(accessTokenPayload);
   }
 
   @Get(':id')
+  @UseGuards(PermissionsGuard) // Apply both guards to this endpoint)
+  @Permissions("user:read") // Only users with 'user:read' permission can access this endpoint
   @ApiGetUser()
-  findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.userService.findById(id);
+  findOne(@AccessToken() accessTokenPayload: AccessTokenPayload, @Param('id', ParseIntPipe) id: number) {
+    return this.userService.findById(accessTokenPayload, id);
   }
 
   @Patch(':id')
   @ApiPatchUser()
   update(
+    @AccessToken() accessTokenPayload: AccessTokenPayload,
     @Param('id', ParseIntPipe) id: number, 
     @Body() updateUserDto: UpdateUserDto
   ) {
-    return this.userService.update(id, updateUserDto);
+    return this.userService.update(accessTokenPayload, id, updateUserDto);
   }
 
   @Delete(':id')
   @ApiDeleteUser()
-  remove(@Param('id', ParseIntPipe) id: number) {
-    return this.userService.remove(id);
+  remove(@AccessToken() accessTokenPayload: AccessTokenPayload, @Param('id', ParseIntPipe) id: number) {
+    return this.userService.remove(accessTokenPayload, id);
   }
 }
