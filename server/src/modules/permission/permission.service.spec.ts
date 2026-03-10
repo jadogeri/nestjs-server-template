8a718828a39387f35c4550b2581cc3ee7644ad7c
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-03-09 23:19:51 -0500

    Updated PermissionService

--- server/src/modules/permission/permission.service.spec.ts
+++ server/src/modules/permission/permission.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PermissionService } from './permission.service';
 import { PermissionRepository } from './permission.repository';
 import { CaslAbilityFactory } from '../../core/security/casl/casl-ability.service';
+import { CacheService } from '../../core/infrastructure/cache/cache.service';
 
 describe('PermissionService', () => {
   let service: PermissionService;
@@ -17,6 +18,10 @@ describe('PermissionService', () => {
         {
           provide: CaslAbilityFactory,
           useValue: { createForUser: jest.fn() },
+        },
+        {
+          provide: CacheService,
+          useValue: { get: jest.fn(), set: jest.fn(), delete: jest.fn() },
         }
       ],
     }).compile();
